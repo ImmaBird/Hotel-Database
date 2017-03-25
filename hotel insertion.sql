a9a1734511e31997b52db5ee2bde32c18ad0a69f
Andrew Mellen <mellena1@wit.edu>
2017-03-25 17:02:30 -0400

    insertion file

---
+++ hotel insertion.sql
@@ -0,0 +1,7 @@
+Insert into Hotel Values ('00001', '10 Main Street', 'Boston', 'MA', '02110', '(617)111-1111');
+
+Insert into Customer Values ('00000001', 'Bob Smith', '155 Light Street', 'Boston', 'MA', '02110', '(617)222-2222', '1111-1111-1111-1111', '09/19', '222', 10252);
+
+Insert into Room Values ('152', '00001', 4, 2, 1543.96);
+
+Insert into Reservation Values ('12345678', '00000001', '00001', '152', 02-25-2017, 05-22-2017, 05-24-2017);
