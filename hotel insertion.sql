e6a9d27c09f292c3609b76e3383b47d0d8ace99f
Bradley Rawson <immapokeyew@gmail.com>
2017-03-25 18:49:18 -0400

    hi

--- hotel insertion.sql
+++ hotel insertion.sql
@@ -103,9 +103,46 @@ insert into Room values  (1, '00001', 1, 1, '1246.18'),
 
 Insert into Reservation Values ('12345678', '00000001', '00001', '152', 02-25-2017, 05-22-2017, 05-24-2017);
 
-Insert into Employee Values ('11111111', '00001', 'Ann Curry', 10-21-1978, '32 High Road', 'MA', '02110', '(617)124-3223'),
-                            ('11111112', '00001', 'Joe Little', 08-12-1968, '12 Low Drive', 'MA', '02110', '(617)222-6442'),
-                            ('11111113', '00001', 'Jim Doe', 03-27-1981, '122 Kite Drive', 'MA', '02110', '(617)111-2322')
+insert into Employee values ('00001', '00002', 'Timothy', '12/25/1962', 'Dwight', 'Pompano Beach', 'FL', '33069', '1-(754)965-6560');
+insert into Employee values ('00002', '00003', 'Timothy', '06/06/1973', 'Sunbrook', 'Washington', 'DC', '20546', '1-(202)594-9060');
+insert into Employee values ('00003', '00004', 'Joseph', '05/23/1966', 'Novick', 'Louisville', 'KY', '40250', '1-(502)768-8549');
+insert into Employee values ('00004', '00001', 'Phyllis', '04/07/1985', 'Waubesa', 'Woburn', 'MA', '01813', '1-(339)959-8498');
+insert into Employee values ('00005', '00002', 'Jonathan', '12/17/1951', 'Sugar', 'Rochester', 'NY', '14646', '1-(585)408-9230');
+insert into Employee values ('00006', '00003', 'Katherine', '03/22/1976', 'Ohio', 'Herndon', 'VA', '22070', '1-(757)951-8264');
+insert into Employee values ('00007', '00004', 'Wayne', '01/21/1956', 'Roth', 'Jersey City', 'NJ', '07305', '1-(201)452-1508');
+insert into Employee values ('00008', '00001', 'Harry', '05/04/1984', 'Shelley', 'Los Angeles', 'CA', '90065', '1-(818)161-9810');
+insert into Employee values ('00009', '00002', 'Gary', '09/06/1981', 'Arapahoe', 'Pasadena', 'CA', '91109', '1-(818)681-8919');
+insert into Employee values ('00010', '00003', 'Diana', '04/18/1975', 'American Ash', 'Detroit', 'MI', '48275', '1-(313)155-4698');
+insert into Employee values ('00011', '00004', 'Shirley', '01/24/1954', 'Spaight', 'Atlanta', 'GA', '30380', '1-(404)895-8595');
+insert into Employee values ('00012', '00001', 'Theresa', '08/03/1974', 'Meadow Ridge', 'Washington', 'DC', '20099', '1-(202)894-2099');
+insert into Employee values ('00013', '00002', 'Clarence', '03/17/1981', 'Sheridan', 'Houston', 'TX', '77030', '1-(713)440-5990');
+insert into Employee values ('00014', '00003', 'Lori', '11/28/1959', 'Kenwood', 'Norwalk', 'CT', '06854', '1-(203)415-3671');
+insert into Employee values ('00015', '00004', 'Michelle', '03/16/1963', 'Summer Ridge', 'Corpus Christi', 'TX', '78475', '1-(361)659-4712');
+insert into Employee values ('00016', '00001', 'Linda', '11/18/1971', 'Stang', 'Tucson', 'AZ', '85737', '1-(520)219-6203');
+insert into Employee values ('00017', '00002', 'Susan', '04/28/1987', 'Summit', 'Lake Worth', 'FL', '33462', '1-(561)600-6183');
+insert into Employee values ('00018', '00003', 'Amanda', '08/02/1959', 'Bluejay', 'Birmingham', 'AL', '35279', '1-(205)745-3483');
+insert into Employee values ('00019', '00004', 'Virginia', '07/12/1974', 'Helena', 'New York City', 'NY', '10175', '1-(646)469-2946');
+insert into Employee values ('00020', '00001', 'Carol', '01/05/1986', 'Ridgeview', 'Chicago', 'IL', '60663', '1-(312)539-3183');
+insert into Employee values ('00021', '00002', 'Doris', '03/17/1968', 'Brown', 'Corpus Christi', 'TX', '78405', '1-(361)462-1232');
+insert into Employee values ('00022', '00003', 'Norma', '01/25/1951', 'Gina', 'Corona', 'CA', '92883', '1-(951)156-8485');
+insert into Employee values ('00023', '00004', 'Robert', '11/17/1952', '8th', 'El Paso', 'TX', '79984', '1-(915)533-1397');
+insert into Employee values ('00024', '00001', 'Ronald', '05/02/1984', 'Menomonie', 'Indianapolis', 'IN', '46221', '1-(317)824-0887');
+insert into Employee values ('00025', '00002', 'Stephanie', '08/08/1976', 'Stoughton', 'Bowie', 'MD', '20719', '1-(240)316-7031');
+insert into Employee values ('00026', '00003', 'Edward', '09/17/1985', 'Russell', 'Pueblo', 'CO', '81015', '1-(719)606-3316');
+insert into Employee values ('00027', '00004', 'Peter', '12/01/1975', 'Florence', 'Kansas City', 'MO', '64153', '1-(816)665-7229');
+insert into Employee values ('00028', '00001', 'Jimmy', '12/07/1975', 'Oak', 'Roanoke', 'VA', '24014', '1-(540)811-3156');
+insert into Employee values ('00029', '00002', 'Ronald', '07/24/1977', 'Tony', 'Washington', 'DC', '20599', '1-(202)757-8418');
+insert into Employee values ('00030', '00003', 'Jennifer', '05/21/1953', 'Village Green', 'San Francisco', 'CA', '94177', '1-(415)621-1749');
+insert into Employee values ('00031', '00004', 'Patrick', '08/06/1976', 'Old Shore', 'Cincinnati', 'OH', '45223', '1-(513)652-9733');
+insert into Employee values ('00032', '00001', 'Paul', '12/14/1962', 'Moland', 'Pocatello', 'ID', '83206', '1-(208)865-6327');
+insert into Employee values ('00033', '00002', 'Jose', '09/07/1965', 'Mendota', 'Philadelphia', 'PA', '19151', '1-(215)829-5794');
+insert into Employee values ('00034', '00003', 'Sarah', '04/09/1962', 'Derek', 'Shawnee Mission', 'KS', '66286', '1-(913)694-2511');
+insert into Employee values ('00035', '00004', 'Pamela', '01/15/1955', '4th', 'Vancouver', 'WA', '98687', '1-(360)745-8188');
+insert into Employee values ('00036', '00001', 'Johnny', '06/06/1972', 'Hallows', 'Cedar Rapids', 'IA', '52405', '1-(319)953-9802');
+insert into Employee values ('00037', '00002', 'Antonio', '11/30/1969', 'Northridge', 'Boston', 'MA', '02216', '1-(617)474-6683');
+insert into Employee values ('00038', '00003', 'Richard', '08/05/1983', 'Carioca', 'Richmond', 'CA', '94807', '1-(510)485-8158');
+insert into Employee values ('00039', '00004', 'Paul', '01/23/1980', 'Dorton', 'Stockton', 'CA', '95205', '1-(209)490-6263');
+insert into Employee values ('00040', '00001', 'Janet', '05/18/1960', 'Longview', 'Denver', 'CO', '80249', '1-(303)292-4265');
 
 Insert into Housekeeper Values ('10','11111111'),
 
