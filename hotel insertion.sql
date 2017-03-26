e0bdbb89b59073967b0116a1e0d118efb8bdd788
Bradley Rawson <immapokeyew@gmail.com>
2017-03-26 14:12:56 -0400

    hi

--- hotel insertion.sql
+++ hotel insertion.sql
@@ -99,46 +99,47 @@ insert into Room Values  ('1', '00001', 1, 1, '1402.68'),
 
 Insert into Reservation Values ('12345678', '00000001', '00001', '152', 02-25-2017, 05-22-2017, 05-24-2017),
 
-insert into Employee values ('00001', '00002', 'Timothy', '12/25/1962', 'Dwight', 'Pompano Beach', 'FL', '33069', '1-(754)965-6560');
-insert into Employee values ('00002', '00003', 'Timothy', '06/06/1973', 'Sunbrook', 'Washington', 'DC', '20546', '1-(202)594-9060');
-insert into Employee values ('00003', '00004', 'Joseph', '05/23/1966', 'Novick', 'Louisville', 'KY', '40250', '1-(502)768-8549');
-insert into Employee values ('00004', '00001', 'Phyllis', '04/07/1985', 'Waubesa', 'Woburn', 'MA', '01813', '1-(339)959-8498');
-insert into Employee values ('00005', '00002', 'Jonathan', '12/17/1951', 'Sugar', 'Rochester', 'NY', '14646', '1-(585)408-9230');
-insert into Employee values ('00006', '00003', 'Katherine', '03/22/1976', 'Ohio', 'Herndon', 'VA', '22070', '1-(757)951-8264');
-insert into Employee values ('00007', '00004', 'Wayne', '01/21/1956', 'Roth', 'Jersey City', 'NJ', '07305', '1-(201)452-1508');
-insert into Employee values ('00008', '00001', 'Harry', '05/04/1984', 'Shelley', 'Los Angeles', 'CA', '90065', '1-(818)161-9810');
-insert into Employee values ('00009', '00002', 'Gary', '09/06/1981', 'Arapahoe', 'Pasadena', 'CA', '91109', '1-(818)681-8919');
-insert into Employee values ('00010', '00003', 'Diana', '04/18/1975', 'American Ash', 'Detroit', 'MI', '48275', '1-(313)155-4698');
-insert into Employee values ('00011', '00004', 'Shirley', '01/24/1954', 'Spaight', 'Atlanta', 'GA', '30380', '1-(404)895-8595');
-insert into Employee values ('00012', '00001', 'Theresa', '08/03/1974', 'Meadow Ridge', 'Washington', 'DC', '20099', '1-(202)894-2099');
-insert into Employee values ('00013', '00002', 'Clarence', '03/17/1981', 'Sheridan', 'Houston', 'TX', '77030', '1-(713)440-5990');
-insert into Employee values ('00014', '00003', 'Lori', '11/28/1959', 'Kenwood', 'Norwalk', 'CT', '06854', '1-(203)415-3671');
-insert into Employee values ('00015', '00004', 'Michelle', '03/16/1963', 'Summer Ridge', 'Corpus Christi', 'TX', '78475', '1-(361)659-4712');
-insert into Employee values ('00016', '00001', 'Linda', '11/18/1971', 'Stang', 'Tucson', 'AZ', '85737', '1-(520)219-6203');
-insert into Employee values ('00017', '00002', 'Susan', '04/28/1987', 'Summit', 'Lake Worth', 'FL', '33462', '1-(561)600-6183');
-insert into Employee values ('00018', '00003', 'Amanda', '08/02/1959', 'Bluejay', 'Birmingham', 'AL', '35279', '1-(205)745-3483');
-insert into Employee values ('00019', '00004', 'Virginia', '07/12/1974', 'Helena', 'New York City', 'NY', '10175', '1-(646)469-2946');
-insert into Employee values ('00020', '00001', 'Carol', '01/05/1986', 'Ridgeview', 'Chicago', 'IL', '60663', '1-(312)539-3183');
-insert into Employee values ('00021', '00002', 'Doris', '03/17/1968', 'Brown', 'Corpus Christi', 'TX', '78405', '1-(361)462-1232');
-insert into Employee values ('00022', '00003', 'Norma', '01/25/1951', 'Gina', 'Corona', 'CA', '92883', '1-(951)156-8485');
-insert into Employee values ('00023', '00004', 'Robert', '11/17/1952', '8th', 'El Paso', 'TX', '79984', '1-(915)533-1397');
-insert into Employee values ('00024', '00001', 'Ronald', '05/02/1984', 'Menomonie', 'Indianapolis', 'IN', '46221', '1-(317)824-0887');
-insert into Employee values ('00025', '00002', 'Stephanie', '08/08/1976', 'Stoughton', 'Bowie', 'MD', '20719', '1-(240)316-7031');
-insert into Employee values ('00026', '00003', 'Edward', '09/17/1985', 'Russell', 'Pueblo', 'CO', '81015', '1-(719)606-3316');
-insert into Employee values ('00027', '00004', 'Peter', '12/01/1975', 'Florence', 'Kansas City', 'MO', '64153', '1-(816)665-7229');
-insert into Employee values ('00028', '00001', 'Jimmy', '12/07/1975', 'Oak', 'Roanoke', 'VA', '24014', '1-(540)811-3156');
-insert into Employee values ('00029', '00002', 'Ronald', '07/24/1977', 'Tony', 'Washington', 'DC', '20599', '1-(202)757-8418');
-insert into Employee values ('00030', '00003', 'Jennifer', '05/21/1953', 'Village Green', 'San Francisco', 'CA', '94177', '1-(415)621-1749');
-insert into Employee values ('00031', '00004', 'Patrick', '08/06/1976', 'Old Shore', 'Cincinnati', 'OH', '45223', '1-(513)652-9733');
-insert into Employee values ('00032', '00001', 'Paul', '12/14/1962', 'Moland', 'Pocatello', 'ID', '83206', '1-(208)865-6327');
-insert into Employee values ('00033', '00002', 'Jose', '09/07/1965', 'Mendota', 'Philadelphia', 'PA', '19151', '1-(215)829-5794');
-insert into Employee values ('00034', '00003', 'Sarah', '04/09/1962', 'Derek', 'Shawnee Mission', 'KS', '66286', '1-(913)694-2511');
-insert into Employee values ('00035', '00004', 'Pamela', '01/15/1955', '4th', 'Vancouver', 'WA', '98687', '1-(360)745-8188');
-insert into Employee values ('00036', '00001', 'Johnny', '06/06/1972', 'Hallows', 'Cedar Rapids', 'IA', '52405', '1-(319)953-9802');
-insert into Employee values ('00037', '00002', 'Antonio', '11/30/1969', 'Northridge', 'Boston', 'MA', '02216', '1-(617)474-6683');
-insert into Employee values ('00038', '00003', 'Richard', '08/05/1983', 'Carioca', 'Richmond', 'CA', '94807', '1-(510)485-8158');
-insert into Employee values ('00039', '00004', 'Paul', '01/23/1980', 'Dorton', 'Stockton', 'CA', '95205', '1-(209)490-6263');
-insert into Employee values ('00040', '00001', 'Janet', '05/18/1960', 'Longview', 'Denver', 'CO', '80249', '1-(303)292-4265');
+insert into Employee values
+('00001', '00002', 'Terry Hanson', '01/19/1977', '63772 Crest Line Park', 'Indianapolis', 'IN', '46216', '(317)174-2111'),
+('00002', '00003', 'Bobby Moreno', '01/24/1981', '032 Farragut Crossing', 'Portland', 'OR', '97255', '(971)466-6584'),
+('00003', '00004', 'Timothy Reynolds', '07/13/1950', '9 Truax Center', 'Alhambra', 'CA', '91841', '(626)998-0431'),
+('00004', '00001', 'Willie Coleman', '02/05/1981', '001 Claremont Lane', 'Brooklyn', 'NY', '11254', '(212)896-3999'),
+('00005', '00002', 'Howard Wagner', '05/23/1956', '49600 Mitchell Avenue', 'Temple', 'TX', '76505', '(254)981-9544'),
+('00006', '00003', 'Robert Mitchell', '12/03/1977', '92605 Longview Alley', 'Fairbanks', 'AK', '99790', '(907)133-1123'),
+('00007', '00004', 'Anthony Weaver', '12/20/1959', '2925 Moland Point', 'Augusta', 'GA', '30919', '(706)662-0560'),
+('00008', '00001', 'Rebecca Watkins', '05/23/1987', '1680 Mcguire Plaza', 'Raleigh', 'NC', '27658', '(919)149-9060'),
+('00009', '00002', 'Lillian Torres', '04/02/1987', '472 Bultman Pass', 'Buffalo', 'NY', '14205', '(716)965-5090'),
+('00010', '00003', 'Jacqueline Jordan', '11/09/1989', '2713 Judy Hill', 'Virginia Beach', 'VA', '23471', '(757)295-7808'),
+('00011', '00004', 'Amanda Armstrong', '10/10/1980', '8 Bobwhite Crossing', 'Lehigh Acres', 'FL', '33972', '(863)106-0472'),
+('00012', '00001', 'Aaron Spencer', '03/12/1980', '73 Laurel Point', 'Canton', 'OH', '44710', '(330)849-6203'),
+('00013', '00002', 'Jesse Kim', '04/13/1972', '0230 Rieder Terrace', 'Portland', 'OR', '97216', '(503)622-4055'),
+('00014', '00003', 'Rose Miller', '01/15/1953', '9 Colorado Plaza', 'Vancouver', 'WA', '98682', '(360)663-5516'),
+('00015', '00004', 'William Jacobs', '09/16/1957', '44156 Farragut Avenue', 'Evansville', 'IN', '47732', '(812)840-9697'),
+('00016', '00001', 'Norma Garza', '08/28/1965', '1151 Hoepker Park', 'Battle Creek', 'MI', '49018', '(269)349-2643'),
+('00017', '00002', 'Kathy Vasquez', '01/19/1968', '1 Pawling Trail', 'San Bernardino', 'CA', '92410', '(951)404-8222'),
+('00018', '00003', 'Jesse Ramirez', '11/24/1985', '1432 Lake View Road', 'Chesapeake', 'VA', '23324', '(757)500-8549'),
+('00019', '00004', 'Christine Weaver', '03/08/1956', '260 Farragut Trail', 'Chicago', 'IL', '60646', '(847)300-2302'),
+('00020', '00001', 'Edward Jones', '02/15/1960', '9950 Comanche Hill', 'Rochester', 'NY', '14652', '(585)300-9646'),
+('00021', '00002', 'Robert King', '02/19/1958', '5 Bluestem Junction', 'Oklahoma City', 'OK', '73197', '(405)711-6360'),
+('00022', '00003', 'John Warren', '06/04/1984', '1 Fair Oaks Avenue', 'San Francisco', 'CA', '94177', '(415)126-3262'),
+('00023', '00004', 'Virginia Cook', '12/04/1954', '026 Ludington Point', 'Jackson', 'MS', '39210', '(601)987-8339'),
+('00024', '00001', 'Johnny Freeman', '06/16/1985', '9208 Moland Crossing', 'Athens', 'GA', '30610', '(706)408-5414'),
+('00025', '00002', 'Jerry Hanson', '08/27/1978', '9 Mayer Circle', 'New York City', 'NY', '10131', '(212)688-4250'),
+('00026', '00003', 'Earl Carr', '03/17/1966', '8900 Walton Place', 'Columbia', 'SC', '29215', '(803)363-4673'),
+('00027', '00004', 'Tina Foster', '05/25/1982', '9 Namekagon Center', 'Seattle', 'WA', '98148', '(206)284-9769'),
+('00028', '00001', 'Michael Kennedy', '02/17/1982', '7777 Becker Point', 'Ocala', 'FL', '34479', '(352)505-1102'),
+('00029', '00002', 'Martha Cole', '02/19/1966', '698 Lukken Place', 'Saint Paul', 'MN', '55166', '(651)634-3539'),
+('00030', '00003', 'Jane Hernandez', '08/05/1980', '4419 Havey Court', 'Brockton', 'MA', '02305', '(508)544-6828'),
+('00031', '00004', 'James Cook', '08/14/1964', '0 Hovde Pass', 'Las Vegas', 'NV', '89166', '(702)176-4537'),
+('00032', '00001', 'Beverly Powell', '09/01/1950', '6307 Eliot Road', 'Buffalo', 'NY', '14220', '(716)312-8060'),
+('00033', '00002', 'Antonio Fisher', '05/02/1964', '131 Oak Park', 'Dayton', 'OH', '45454', '(937)139-7452'),
+('00034', '00003', 'Martin Campbell', '01/24/1965', '403 Helena Point', 'Nashville', 'TN', '37220', '(615)200-2104'),
+('00035', '00004', 'Craig Hicks', '06/08/1967', '21 Lighthouse Bay Hill', 'Littleton', 'CO', '80126', '(303)866-4094'),
+('00036', '00001', 'Roger Rodriguez', '02/09/1954', '6314 Heath Way', 'North Las Vegas', 'NV', '89087', '(702)810-6365'),
+('00037', '00002', 'Albert Griffin', '07/30/1976', '01915 Harbort Street', 'Lexington', 'KY', '40505', '(859)800-0475'),
+('00038', '00003', 'Martha Black', '06/11/1986', '21 Sundown Trail', 'Washington', 'DC', '20392', '(202)753-6925'),
+('00039', '00004', 'Tammy Lopez', '08/08/1975', '84366 Waubesa Circle', 'Springfield', 'MA', '01129', '(413)334-8262'),
+('00040', '00001', 'Patrick Anderson', '06/02/1977', '55 Emmet Avenue', 'Houston', 'TX', '77080', '(832)984-5378');
 
 Insert into Housekeeper Values ('10','11111111'),
 
