49c8d1f8ab60cf4d459580fe59e201c48d1c336a
Andrew Mellen <mellena1@wit.edu>
2017-03-26 20:34:04 -0400

    queries

---
+++ Queries.sql
@@ -0,0 +1,9 @@
+#Simple (5 Queries Lecture 6)
+Select * From Hotel;
+SELECT Name, Street, City, State, ZipCode from Employee;
+SELECT Name from Customer where RewardPoints > 10000;
+Select HotelID, RoomNumber from Room where MaxOccupancy >= 4;
+
+#Intermediate (3 Queries Lecture 7)
+
+#Complex (3 Queries Lecture 8)
